42195eccb4c88fe2c5e2a7cd91913c790e794db8
Meet Oza <ozamee17@gmail.com>
2024-10-09 23:30:28 +0530

    SIGNUP

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/signup')
+@app.route('/sign_up')
 def sign_up():
-    return render_template('sign-up.html')
+    return render_template('sign_up.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
