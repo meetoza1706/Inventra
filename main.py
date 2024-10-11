6eedb6428ba3821f76cd571535e902bd22da0145
Meet Oza <ozamee17@gmail.com>
2024-10-12 02:35:16 +0530

    ajax for login and sign_up works up and running now time for production to test <3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,13 +6,30 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/sign_up')
+@app.route('/sign_up', methods=['POST','GET'])
 def sign_up():
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    cpassword = request.form.get('cpassword')
+
+    if username:
+        print(username,email,password,cpassword)
+        return jsonify({'message': f'Success for {username}.'})
+    
     return render_template('sign_up.html')
 
-@app.route('/login')
+@app.route('/login', methods=['POST','GET'])
 def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    
+    if username and password:
+        print(username, password)
+        return jsonify({'message': f'Success for {username}.'})
+    
     return render_template('login.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
